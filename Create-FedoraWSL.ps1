1e86539b3b87735b591653f787982b8b99177e78
Adam Chance <adam.chance10@live.co.uk>
2020-11-11 10:16:31 +0000

    Request the UNIX Username on one line

--- Create-FedoraWSL.ps1
+++ Create-FedoraWSL.ps1
@@ -6,6 +6,6 @@ wsl --import Fedora-33 C:\WSL\Fedora-33 layer.tar
 wsl -d Fedora-33 -e dnf -y update
 wsl -d Fedora-33 -e dnf -y install sudo passwd
 
-wsl -d Fedora-33 -e bash -c "echo UNIX Username: ; read unixusername ; useradd -G wheel `$unixusername ; passwd `$unixusername"
+wsl -d Fedora-33 -e bash -c "printf 'UNIX Username: ' ; read unixusername ; useradd -G wheel `$unixusername ; passwd `$unixusername"
 
 Get-ItemProperty Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Lxss\*\ DistributionName | Where-Object -Property DistributionName -eq Fedora-33  | Set-ItemProperty -Name DefaultUid -Value 1000
