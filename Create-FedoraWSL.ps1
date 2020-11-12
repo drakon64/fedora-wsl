083fd3a2afa6739c8306fde1b4698754b94091ef
Adam Chance <adam.chance10@live.co.uk>
2020-11-12 16:08:20 +0000

    Optionally set the new distribution as the default

--- Create-FedoraWSL.ps1
+++ Create-FedoraWSL.ps1
@@ -1,6 +1,7 @@
 Param (
 	[String] $Distribution = "Fedora-33",
-	[System.IO.DirectoryInfo] $Path = "C:\WSL\"
+	[System.IO.DirectoryInfo] $Path = "C:\WSL\",
+	[Switch] $SetDefault
 )
 
 Invoke-WebRequest -Uri https://dl.fedoraproject.org/pub/fedora/linux/releases/33/Container/x86_64/images/Fedora-Container-Base-33-1.2.x86_64.tar.xz -OutFile Fedora-Container-Base-33-1.2.x86_64.tar.xz
@@ -21,3 +22,7 @@ wsl --distribution "$Distribution" --exec dnf -y --releasever 33 install shadow-
 wsl --distribution "$Distribution" --exec bash -c "printf 'UNIX Username: ' ; read unixusername ; useradd -G wheel `$unixusername ; passwd `$unixusername"
 
 Get-ItemProperty Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Lxss\*\ DistributionName | Where-Object -Property DistributionName -eq "$Distribution"  | Set-ItemProperty -Name DefaultUid -Value 1000
+
+If ($SetDefault) {
+	wsl --set-default "$Distribution"
+}
