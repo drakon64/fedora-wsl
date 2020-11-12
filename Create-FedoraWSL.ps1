db8bc816e1dcc5f373574b085e33aec457a80622
Adam Chance <adam.chance10@live.co.uk>
2020-11-12 18:00:54 +0000

    Don't install DNF Config Manager

--- Create-FedoraWSL.ps1
+++ Create-FedoraWSL.ps1
@@ -17,7 +17,7 @@ wsl --import "$Distribution" "$Path" layer.tar
 wsl --distribution "$Distribution" --exec cp --reflink=auto /etc/yum.repos.d/fedora.repo /etc/yum.repos.d/fedora.repo.rpmsave
 wsl --distribution "$Distribution" --exec dnf -y remove fedora-logos fedora-release fedora-release-notes
 wsl --distribution "$Distribution" --exec mv /etc/yum.repos.d/fedora.repo.rpmsave /etc/yum.repos.d/fedora.repo
-wsl --distribution "$Distribution" --exec dnf -y --releasever 33 install shadow-utils passwd cracklib-dicts sudo 'dnf-command(config-manager)' generic-logos generic-release generic-release-notes
+wsl --distribution "$Distribution" --exec dnf -y --releasever 33 install shadow-utils passwd cracklib-dicts sudo generic-logos generic-release generic-release-notes
 
 wsl --distribution "$Distribution" --exec bash -c "printf 'UNIX Username: ' ; read unixusername ; useradd -G wheel `$unixusername ; passwd `$unixusername"
 
