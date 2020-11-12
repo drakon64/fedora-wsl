888e48bb33eb9dd357bf8902727ac00bcc561345
Adam Chance <adam.chance10@live.co.uk>
2020-11-12 13:42:40 +0000

    Use long parameter names

--- Create-FedoraWSL.ps1
+++ Create-FedoraWSL.ps1
@@ -12,12 +12,12 @@ If (!(Test-Path -Path "$Path"\"$Distribution" -PathType Container)) {
 }
 
 wsl --import "$Distribution" "$Path"\"$Distribution" layer.tar
-wsl -d "$Distribution" -e sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/fedora-updates.repo
-wsl -d "$Distribution" -e sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/fedora-updates-modular.repo
-wsl -d "$Distribution" -e dnf -y install sudo passwd cracklib-dicts 'dnf-command(config-manager)'
-wsl -d "$Distribution" -e dnf config-manager --set-enabled updates --save
-wsl -d "$Distribution" -e dnf config-manager --set-enabled updates-modular --save
+wsl --distribution "$Distribution" --exec sed --in-place 's/enabled=1/enabled=0/g' /etc/yum.repos.d/fedora-updates.repo
+wsl --distribution "$Distribution" --exec sed --in-place 's/enabled=1/enabled=0/g' /etc/yum.repos.d/fedora-updates-modular.repo
+wsl --distribution "$Distribution" --exec dnf --yes install sudo passwd cracklib-dicts 'dnf-command(config-manager)'
+wsl --distribution "$Distribution" --exec dnf config-manager --set-enabled updates --save
+wsl --distribution "$Distribution" --exec dnf config-manager --set-enabled updates-modular --save
 
-wsl -d "$Distribution" -e bash -c "printf 'UNIX Username: ' ; read unixusername ; useradd -G wheel `$unixusername ; passwd `$unixusername"
+wsl --distribution "$Distribution" --exec bash -c "printf 'UNIX Username: ' ; read unixusername ; useradd -G wheel `$unixusername ; passwd `$unixusername"
 
 Get-ItemProperty Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Lxss\*\ DistributionName | Where-Object -Property DistributionName -eq "$Distribution"  | Set-ItemProperty -Name DefaultUid -Value 1000
