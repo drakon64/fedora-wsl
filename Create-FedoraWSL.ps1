fc7bef6237d6e936500f6a9a01be27fa720078f5
Adam Chance <adam.chance10@live.co.uk>
2020-11-12 18:32:44 +0000

    Download a temporary xz if it is not on the PATH

--- Create-FedoraWSL.ps1
+++ Create-FedoraWSL.ps1
@@ -5,7 +5,24 @@ Param (
 )
 
 Invoke-WebRequest -Uri https://dl.fedoraproject.org/pub/fedora/linux/releases/33/Container/x86_64/images/Fedora-Container-Base-33-1.2.x86_64.tar.xz -OutFile Fedora-Container-Base-33-1.2.x86_64.tar.xz
-xz --decompress --force .\Fedora-Container-Base-33-1.2.x86_64.tar.xz
+
+If (Get-Command -Name xz -CommandType Application) {
+	xz --decompress --force .\Fedora-Container-Base-33-1.2.x86_64.tar.xz
+}
+Else {
+	$XzZip = New-TemporaryFile
+	Invoke-WebRequest -Uri https://tukaani.org/xz/xz-5.2.5-windows.zip -OutFile $XzZip
+
+	Add-Type -AssemblyName System.IO.Compression.FileSystem
+	$XzZipFile = [System.IO.Compression.ZipFile]::OpenRead($XzZip)
+
+	$Xz = New-TemporaryFile
+	[System.IO.Compression.ZipFileExtensions]::ExtractToFile($XzZipFile.GetEntry("bin_x86-64/xz.exe"), $Xz, $True)
+	$XzZipFile.Dispose()
+
+	Start-Process -FilePath $Xz -ArgumentList "--decompress --force .\Fedora-Container-Base-33-1.2.x86_64.tar.xz" -NoNewWindow -Wait
+	$XzZip, $Xz | Remove-Item
+}
 tar tf .\Fedora-Container-Base-33-1.2.x86_64.tar | Where-Object { $_ -Like "*/layer.tar" } | ForEach-Object { tar xf .\Fedora-Container-Base-33-1.2.x86_64.tar --strip-components=1 "$_" }
 
 [String] $Path = $Path.FullName + $Distribution
